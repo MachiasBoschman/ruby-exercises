2e983062d979d95b9118633f81ce42fce261c47b
Machias Boschman <machias.boschman@gmail.com>
2026-09-02 12:33:23 +0000

    Caesar Cipher project

---
+++ projects/caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,15 @@
+def caesar_cipher(string, shift_amount)
+  string.chars.map { |char| shift_char(char, shift_amount) }.join
+end
+
+def shift_char(char, shift_amount)
+  base = case char
+         when 'a'..'z' then 'a'.ord
+         when 'A'..'Z' then 'A'.ord
+         end
+  return char if base.nil?
+
+  (((char.ord - base + shift_amount) % 26) + base).chr
+end
+
+puts caesar_cipher('What a string!', 5)
